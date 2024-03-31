2e96d29f160c4c42d30fd8b2baa2e0f72dae5c4a
mengenmetehan <metehanmengen@gmail.com>
2024-03-31 21:39:19 +0300

    feature Ingredients api: release 001 : endpoints added

--- Online-food-ordering-app/src/main/java/com/mengen/controller/IngredientsController.java
+++ Online-food-ordering-app/src/main/java/com/mengen/controller/IngredientsController.java
@@ -8,6 +8,8 @@ import com.mengen.service.IngredientsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/admin/ingredients")
 public class IngredientsController {
@@ -25,14 +27,23 @@ public class IngredientsController {
     }
 
     @PostMapping()
-    public ResponseEntity<IngredientsItem> createIngredientCategory(@RequestBody IngredientRequestDTO request) throws Exception {
+    public ResponseEntity<IngredientsItem> createIngredientItem(@RequestBody IngredientRequestDTO request) throws Exception {
 
         return ResponseEntity.ok(ingredientsService.createIngredientItem(request.getRestaurantId(),request.getName(), request.getCategoryId()));
     }
 
     @PutMapping("/{id}/stoke")
     public ResponseEntity<IngredientsItem> updateIngredientStock(@PathVariable Long id) throws Exception {
-
         return ResponseEntity.ok(ingredientsService.updateStock(id));
+    }
+
+    @GetMapping("/restaurant/{id}")
+    public ResponseEntity<List<IngredientsItem>> getRestaurantIngredient(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(ingredientsService.findRestaurantsIngredients(id));
+    }
+
+    @GetMapping("/restaurant/{id}/category")
+    public ResponseEntity<IngredientCategory> getRestaurantIngredientCategory(@PathVariable Long id) throws Exception {
+        return ResponseEntity.ok(ingredientsService.findIngredientCategoryById(id));
     }
 }
